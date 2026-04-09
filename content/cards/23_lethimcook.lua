3f122f82ac898015365893e1019f13f9e82990e2
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-04-09 17:41:05 +0100

    let him cook

--- content/cards/23_lethimcook.lua
+++ content/cards/23_lethimcook.lua
@@ -14,7 +14,7 @@ SMODS.Joker {
 
 local calc_main_scoring_ref = SMODS.calculate_main_scoring
 function SMODS.calculate_main_scoring(context, scoring_hand)
-    if context.cardarea == G.play and next(SMODS.find_card('j_kh_lethimcook')) and SMODS.calculate_round_score() > G.GAME.blind.chips and G.GAME.current_round.hands_played == 0 then
+    if context.cardarea == G.play and next(SMODS.find_card('j_kh_lethimcook')) then
 		-- call normal scoring
         calc_main_scoring_ref(context, scoring_hand)
         
@@ -22,13 +22,15 @@ function SMODS.calculate_main_scoring(context, scoring_hand)
         local original_scoring_hand = context.scoring_hand
 
 		-- call scoring again in a random order
-        for _, joker in ipairs(SMODS.find_card('j_kh_lethimcook')) do
-			local rnd_order = XIII.shuffle_cards(scoring_hand, "randomiser")
-			context.cardarea = { cards = rnd_order }
-			context.scoring_hand = rnd_order
-            SMODS.calculate_effect({message = localize('k_kh_rescore_ex'), colour = G.C.SECONDARY_SET.Enhanced}, joker)
-			calc_main_scoring_ref(context, rnd_order)
-        end
+		if SMODS.calculate_round_score() > G.GAME.blind.chips and G.GAME.current_round.hands_played == 0 then
+			for _, joker in ipairs(SMODS.find_card('j_kh_lethimcook')) do
+				local rnd_order = XIII.shuffle_cards(scoring_hand, "randomiser")
+				context.cardarea = { cards = rnd_order }
+				context.scoring_hand = rnd_order
+				SMODS.calculate_effect({message = localize('k_kh_rescore_ex'), colour = G.C.SECONDARY_SET.Enhanced}, joker)
+				calc_main_scoring_ref(context, rnd_order)
+			end
+		end
 		
 		-- make it back to normal
         context.cardarea = original_cardarea
