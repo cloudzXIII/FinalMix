ed8d2469767c61ccd9eb42b1ad9980fb9866174b
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-04-09 18:49:21 +0100

    Finally fix moogle voucher
    fix softlock when skipping into a booster pack tag

--- content/misc/vouchers.lua
+++ content/misc/vouchers.lua
@@ -18,31 +18,23 @@ MIX.send_to_room = function(area)
     G.E_MANAGER:add_event(Event {
         trigger = "after",
         func = function()
-            G.E_MANAGER:add_event(Event {
-                trigger = "after",
-                func = function()
-                    if G.STATE ~= G.STATES.SMODS_BOOSTER_OPENED then
-                        G.STATE = area
-                        G.STATE_COMPLETE = false
-                        return true
-                    end
-                end
-            })
-            if G.blind_select then
-                G.blind_select.alignment.offset.y = G.blind_select.alignment.offset.y + G.blind_select.T.h
-                G.E_MANAGER:add_event(Event {
-                    trigger = "after",
-                    delay = 0.3,
-                    func = function()
-                        G.blind_select:remove()
-                        G.blind_prompt_box:remove()
-                        return true
-                    end
-                })
-            end
+            G.STATE = area
+            G.STATE_COMPLETE = false
             return true
         end
     })
+    if G.blind_select then
+        G.blind_select.alignment.offset.y = G.blind_select.alignment.offset.y + G.blind_select.T.h
+        G.E_MANAGER:add_event(Event {
+            trigger = "after",
+            delay = 0.3,
+            func = function()
+                G.blind_select:remove()
+                G.blind_prompt_box:remove()
+                return true
+            end
+        })
+    end
 end
 
 SMODS.Voucher {
@@ -55,9 +47,15 @@ SMODS.Voucher {
     loc_vars = function(self, info_queue, card)
     end,
     requires = { 'v_kh_moogleskip' },
-    calculate = function(self, card, context)
-        if context.skip_blind then
-            MIX.send_to_room(G.STATES.SHOP)
-        end
-    end
+    redeem = function(self, card)
+        G.GAME.kh_moogleshop = true
+    end,
 }
+
+local ref_skip_blind = G.FUNCS.skip_blind
+function G.FUNCS.skip_blind(e)
+    if G.GAME.kh_moogleshop then
+        MIX.send_to_room(G.STATES.SHOP)
+    end
+    return ref_skip_blind(e)
+end
